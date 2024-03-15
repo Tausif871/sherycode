bc2d66953b5115ec6f765e10319b20a0cae63d7f
Tausif Alam <iamtausif871@gmail.com>
2024-03-15 23:24:14 +0530

    js started and completed introduction part

---
+++ js/1 intro/script.js
@@ -0,0 +1,105 @@
+
+
+/* 
+
+here starting js lectures -->
+
+      <!-- what is js ? -->
+
+      <!-- js is a scripting languages. scripting language used as the only language of web.  -->
+
+      <!-- 
+        
+      there are 3 types of languages 
+
+      1. markup languages [ ex - hmtl & xml,markup language is that contain tags and jisko likh doge wo ajayega and it does not get error.]
+
+      2. scripting languages [ ex - js, Perl, Ruby, PHP, pyhton]
+
+      The scripting languages are interpreter-based languages
+
+      3. programming languages [ ex - C, C++, COBOL, Basic, VB, C#, Pascal, Java]
+
+      The programming languages are compiler-based languages.
+    
+      -->
+
+
+      <!-- 
+        
+      why js?
+      
+      this is why learn js --
+
+      html - content
+      css - styles
+      js - add functionalities.
+
+
+
+      -->
+
+
+      <!-- 
+
+        why we use js ?
+
+        just create a script tag in html and attach js file to their src like css and when html file loads so js file loads too. after that  place the file in body section at the bottom or last point of html code of the body.
+
+
+       -->
+
+
+       <!-- 
+
+        how we run js ?
+
+        just write the code and reload the browser.
+
+        -->
+
+
+        <!-- 
+
+          how computer reads and understands the code?
+
+          computer read signle word to word and then match with js code if it find correct it shows no error if not it shows error.
+
+          some time it shows error in developer mode of chrome's console.
+
+          you go and inspect the code file and then go to console it shows error if error occur.
+         -->
+
+         */
+
+
+
+
+
+
+
+// 1. alert part 
+
+alert("hey");
+
+// alert pop a message if the browser load.
+
+
+// 2. prompts - it is use to ask questions and we can save answers in a variable. 
+// it asks a questions and show a input field to give answer.
+
+var ans = prompt("aur kaise ho?");
+
+console.log(ans);
+
+
+// 3. console warn error and log
+
+console.log("hi");
+console.warn("hiiii");
+console.error("hy");
+
+
+
+
+
